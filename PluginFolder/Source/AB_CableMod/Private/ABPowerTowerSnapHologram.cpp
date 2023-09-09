45c701a356c1d5513ba27f34693b7127a3796cd6
davidg <david.h.gillen@gmail.com>
2023-09-09 13:11:36 -0600

    Working Simple Snap Hologram

--- PluginFolder/Source/AB_CableMod/Private/ABPowerTowerSnapHologram.cpp
+++ PluginFolder/Source/AB_CableMod/Private/ABPowerTowerSnapHologram.cpp
@@ -1,22 +1,47 @@
 #include "ABPowerTowerSnapHologram.h"
+#include "AbstractInstanceManager.h"
 #include "FGConstructDisqualifier.h"
+#include "FGClearanceComponent.h"
 
-bool AABPowerTowerSnapHologram::IsValidHitResult(const FHitResult& hitResult) const { return true;  }
+bool AABPowerTowerSnapHologram::IsValidHitResult(const FHitResult& hitResult) const {
+	AActor* hitActor = hitResult.GetActor();
+	if (AAbstractInstanceManager* hitAbstract = Cast<AAbstractInstanceManager>(hitActor)) {
+		FInstanceHandle instHandle;
+		hitAbstract->ResolveHit(hitResult, instHandle);
+		hitActor = instHandle.GetOwner<AActor>();
+	}
 
-void AABPowerTowerSnapHologram::SetHologramLocationAndRotation(const FHitResult& hitResult) {
-	//return Super::SetHologramLocationAndRotation(hitResult);
-
-	bool bValid = false;
-	AFGBuildable* hitActor = Cast<AFGBuildable>(hitResult.GetActor());
+	AFGBuildable* hitBuildable = Cast<AFGBuildable>(hitActor);
+	if (hitBuildable == NULL) { return false; }
 
 	for (int i = 0, l = mSnappableTo.Num(); i < l; i++) {
-		if (hitActor->StaticClass()->IsChildOf(mSnappableTo[i].Get()->StaticClass())) { bValid = true; break; }
+		if (hitActor->GetClass()->IsChildOf(mSnappableTo[i]->GetDefaultObject()->GetClass())) { return true; }
+	}
+
+	return false;
+}
+
+void AABPowerTowerSnapHologram::SetHologramLocationAndRotation(const FHitResult& hitResult) {
+	AActor* hitActor = hitResult.GetActor();
+	if (AAbstractInstanceManager* hitAbstract = Cast<AAbstractInstanceManager>(hitActor)) {
+		FInstanceHandle instHandle;
+		hitAbstract->ResolveHit(hitResult, instHandle);
+		hitActor = instHandle.GetOwner<AActor>();
+	}
+
+	AFGBuildable* hitBuildable = Cast<AFGBuildable>(hitActor);
+	if (hitBuildable == NULL) { return; }
+
+	bool bValid = false;
+	for (int i = 0, l = mSnappableTo.Num(); i < l; i++) {
+		if (hitActor->GetClass()->IsChildOf(mSnappableTo[i]->GetDefaultObject()->GetClass())) { bValid = true; break; }
 	}
 
 	if(bValid) {
-		UFGClearanceComponent* clearanceSnap = hitActor->GetClearanceComponent();
-		SetActorLocation(hitActor->GetActorLocation());
-		SetActorRotation(hitActor->GetActorRotation());
+		UFGClearanceComponent* clearanceSnap = hitBuildable->GetClearanceComponent();
+		FVector offset = FVector(0.0f, 0.0f, clearanceSnap->GetScaledBoxExtent().Z);
+		SetActorLocation(clearanceSnap->GetComponentLocation() + offset);
+		SetActorRotation(hitBuildable->GetActorRotation());
 	} else {
 		SetActorLocation(hitResult.Location);
 		AddConstructDisqualifier(UFGCDInvalidAimLocation::StaticClass());
