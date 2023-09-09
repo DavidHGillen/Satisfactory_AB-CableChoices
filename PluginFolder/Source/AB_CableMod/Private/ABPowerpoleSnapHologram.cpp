866d7ad2453aab1f8434c19b8681bd2fa93c50ab
davidg <david.h.gillen@gmail.com>
2023-09-09 16:34:43 -0600

    Corrected general purpose snapable powerpole

---
+++ PluginFolder/Source/AB_CableMod/Private/ABPowerpoleSnapHologram.cpp
@@ -0,0 +1,78 @@
+#include "ABPowerpoleSnapHologram.h"
+#include "AbstractInstanceManager.h"
+#include "FGConstructDisqualifier.h"
+#include "FGClearanceComponent.h"
+
+bool AABPowerpoleSnapHologram::IsMatchingType(const FHitResult& hitResult) const {
+	AActor* hitActor = hitResult.GetActor();
+
+	if (AAbstractInstanceManager* hitAbstract = Cast<AAbstractInstanceManager>(hitActor)) {
+		FInstanceHandle instHandle;
+		hitAbstract->ResolveHit(hitResult, instHandle);
+		hitActor = instHandle.GetOwner<AActor>();
+	}
+
+	if (hitActor == NULL) { return false;  }
+
+	for (int i = 0, l = mSnappableTo.Num(); i < l; i++) {
+		UClass* testClass = mSnappableTo[i]->GetDefaultObject()->GetClass();
+		if (hitActor->GetClass()->IsChildOf(testClass)) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+void AABPowerpoleSnapHologram::CheckValidFloor() {
+	if (mSnappedBuilding != NULL) { return; }
+
+	Super::CheckValidFloor();
+}
+
+bool AABPowerpoleSnapHologram::IsValidHitResult(const FHitResult& hitResult) const {
+	if (bExclusiveSnap) {
+		return IsMatchingType(hitResult);
+	}
+
+	return Super::IsValidHitResult(hitResult) || IsMatchingType(hitResult);
+}
+
+void AABPowerpoleSnapHologram::SetHologramLocationAndRotation(const FHitResult& hitResult) {
+	bool bValid = IsMatchingType(hitResult);
+	mSnappedBuilding = NULL;
+
+	if (!bValid) {
+		if (bExclusiveSnap) {
+			SetActorLocation(hitResult.Location);
+			AddConstructDisqualifier(UFGCDInvalidAimLocation::StaticClass());
+			return;
+		}
+	} else {
+		AFGBuildable* hitBuildable = Cast<AFGBuildable>(hitResult.GetActor());
+		if (hitBuildable != NULL && eSnapType != EABPoleSnapType::PST_None) {
+			UFGClearanceComponent* clearanceSnap;
+			FRotator rOffset;
+			FVector lOffset;
+
+			mSnappedBuilding = hitBuildable;
+
+			switch (eSnapType) {
+				case EABPoleSnapType::PST_HatSnap:
+					clearanceSnap = hitBuildable->GetClearanceComponent();
+					lOffset = clearanceSnap->GetRelativeLocation() + FVector(0.0f, 0.0f, clearanceSnap->GetScaledBoxExtent().Z);
+					SetActorLocation(hitBuildable->GetTransform().TransformPosition(lOffset));
+					SetActorRotation(hitBuildable->GetActorRotation());
+					return;
+
+				case EABPoleSnapType::PST_BackSnap:
+					rOffset = FRotator(0.0f, 180.0f, 0.0f);
+					SetActorLocation(hitBuildable->GetActorLocation());
+					SetActorRotation(hitBuildable->GetActorRotation() + rOffset);
+					return;
+			}
+		}
+	}
+
+	return Super::SetHologramLocationAndRotation(hitResult);
+}
---
+++ PluginFolder/Source/AB_CableMod/Public/ABPowerpoleSnapHologram.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Hologram/FGPowerPoleHologram.h"
+#include "ABPowerpoleSnapHologram.generated.h"
+
+UENUM()
+enum class EABPoleSnapType : uint8
+{
+	PST_None,
+	PST_HatSnap, // top of the clearance box
+	PST_BackSnap // spun 180 to be back to back
+};
+
+/**
+ * 
+ */
+UCLASS()
+class AB_CABLEMOD_API AABPowerpoleSnapHologram : public AFGPowerPoleHologram
+{
+	GENERATED_BODY()
+
+public:
+
+	bool IsMatchingType(const FHitResult& hitResult) const;
+
+	virtual void CheckValidFloor() override;
+	virtual bool IsValidHitResult(const FHitResult& hitResult) const override;
+	virtual void SetHologramLocationAndRotation(const FHitResult& hitResult) override;
+
+public:
+
+	UPROPERTY(EditDefaultsOnly, Category = "SnapLogic")
+	bool bExclusiveSnap = false;
+
+	UPROPERTY(EditDefaultsOnly, Category = "SnapLogic")
+	EABPoleSnapType eSnapType;
+
+	UPROPERTY(EditDefaultsOnly, Category = "SnapLogic")
+	TArray< TSubclassOf<AFGBuildable> > mSnappableTo;
+};
