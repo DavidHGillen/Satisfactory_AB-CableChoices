2adab9a7e5c3f1b98d61f7aed342890454d8097b
davidg <david.h.gillen@gmail.com>
2026-05-27 23:45:19 -0600

    Misc junk fixes that have apparently broken delay switches in some insane and novel fashion. Praying 1.2 just fixes it for me because it's not my code that's crashing

--- PluginFolder/Source/AB_CableMod/Public/CC_SwatchData.h
+++ PluginFolder/Source/AB_CableMod/Public/CC_SwatchData.h
@@ -12,3 +12,30 @@ public:
 		mGroupName = LOCTABLE(CC_StringPath, "SwatchGroup_PowerlineStd");
 	}
 };
+
+UCLASS()
+class AB_CABLEMOD_API UFGSwatchGroup_Powerline_AU : public UFGSwatchGroup {
+	GENERATED_BODY()
+public:
+	UFGSwatchGroup_Powerline_AU() {
+		mGroupName = LOCTABLE(CC_StringPath, "SwatchGroup_PowerlineAu");
+	}
+};
+
+UCLASS()
+class AB_CABLEMOD_API UFGSwatchGroup_Powerline_CU : public UFGSwatchGroup {
+	GENERATED_BODY()
+public:
+	UFGSwatchGroup_Powerline_CU() {
+		mGroupName = LOCTABLE(CC_StringPath, "SwatchGroup_PowerlineCu");
+	}
+};
+
+UCLASS()
+class AB_CABLEMOD_API UFGSwatchGroup_Powerline_SI : public UFGSwatchGroup {
+	GENERATED_BODY()
+public:
+	UFGSwatchGroup_Powerline_SI() {
+		mGroupName = LOCTABLE(CC_StringPath, "SwatchGroup_PowerlineSi");
+	}
+};
